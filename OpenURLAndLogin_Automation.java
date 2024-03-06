382a97cd6c5cb13187553753fab195085d826241
T.SASANK <sasanktalapaneni@gmail.com>
2024-03-06 09:51:04 +0530

    Code modified

--- OpenURLAndLogin_Automation.java
+++ OpenURLAndLogin_Automation.java
@@ -1,4 +1,4 @@
-package org.example;
+ipackage org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,23 +9,13 @@ import java.time.Duration;
 public class OpenURLAndLogin_Automation {
     public static void main(String[] args) throws InterruptedException {
         // TODO Auto-generated method stub
-        //System.setProperty("webdriver.chrome.driver", "E:\\K L\\Epham_Dept\\chromedriver.exe");
-        //WebDriver driver=new ChromeDriver();
 
         WebDriver driver=new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://lms.kluniversity.in/login/index.php");
-        driver.findElement(By.id("username")).sendKeys("2100030871");
-        driver.findElement(By.id("password")).sendKeys("Akki@1234");
+        driver.findElement(By.id("username")).sendKeys("2100030529");
+        driver.findElement(By.id("password")).sendKeys("S@si20256");
         driver.findElement(By.id("password")).submit();
         Thread.sleep(1000);
-//        driver.findElement(By.className("usertext")).click();
-//        List<WebElement> menubars = driver.findElements(By.xpath("//a/span"));
-//        for(WebElement option : menubars) {
-//            if(option.getText().equalsIgnoreCase("Log out")){
-//                option.click();
-//                break;
-//            }
-//        }
     }
 }
